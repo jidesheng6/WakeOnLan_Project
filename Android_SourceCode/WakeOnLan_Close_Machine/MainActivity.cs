c67e7e1957b1c6922fd33908e0f9b654c319f158
jidesheng6 <2732423664@qq.com>
2020-01-26 19:50:53 +0800

    Add files via upload

---
+++ Android_SourceCode/WakeOnLan_Close_Machine/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using WakeMain;
+
+namespace WakeOnLan_Close_Machine
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Toast.MakeText(this, "注意：本工具只能在局域网中使用，Mac地址请从电脑工具中获取，关机操作，如果此局域网中有点电脑运行了服务也会被关闭，将来考虑出2.0版本。", ToastLength.Long).Show();
+            EditText Mac_Address_Text = FindViewById<EditText>(Resource.Id.Mac_AddressText);
+            Button OpenButton = FindViewById<Button>(Resource.Id.OpenButton);
+            Button CloseButton = FindViewById<Button>(Resource.Id.CloseButton);
+            OpenButton.Click += (sender, e) =>
+             {
+                 if (Mac_Address_Text.Text!="")
+                 {
+                     MainCode.WakeMachine(Mac_Address_Text.Text.Trim());
+                 }
+                 else
+                 {
+                     Toast.MakeText(this, "傻逼吧你，差点退出了，多少沾丶脑瘫", ToastLength.Long).Show();
+                 }
+                 
+
+
+             };
+            CloseButton.Click += (sender, e) =>
+              {
+                 MainCode.CloseMachines();
+              };
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Android_SourceCode/WakeOnLan_Close_Machine/WakeOnLanMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Linq;
+using System.Collections.Generic;
+using System.Web;
+using System.Text;
+
+namespace WakeMain {
+    public static class MainCode {
+        public static int SendPort = 9;
+        static UdpClient GlobalUdp = new UdpClient();
+        static IPAddress Mask = IPAddress.Parse("255.255.255.255");
+        static IPEndPoint SendPonitInfo = new IPEndPoint(Mask, SendPort);
+        public static byte[] MakeMagicPacket(String Mac_Address)
+        {
+            String Header_Stream = "FFFFFFFFFFFF";
+            byte[] SendPacket = new byte[102];
+            byte[] MacgicHex = new byte[Mac_Address.Length / 2];
+            for (int i = 0; i < 6; i++)
+            {
+                SendPacket[i] = Convert.ToByte(Header_Stream.Substring(i*2,2),16);
+            }
+            for (int j = 0; j < MacgicHex.Length; j++)
+            {
+                MacgicHex[j] = Convert.ToByte(Mac_Address.Substring(j*2,2),16);
+            }
+            for (int k = 6; k < 102; k++)
+            {
+                SendPacket[k] = MacgicHex[k % 6];
+            }
+            return SendPacket;
+        }
+        public  static void WakeMachine(String Mac_Text) {
+            byte[] RawPacket = MakeMagicPacket(Mac_Text);
+
+            GlobalUdp.Send(RawPacket,RawPacket.Length,SendPonitInfo);
+        }
+        public  static void CloseMachines()
+        {
+            byte[] SendClosePoint = Encoding.Default.GetBytes("off");
+            MainCode.SendPort = 7856;
+            MainCode.SendPonitInfo = new IPEndPoint(Mask,SendPort);
+            GlobalUdp.Send(SendClosePoint,SendClosePoint.Length,SendPonitInfo);
+        }
+
+    }
+}
